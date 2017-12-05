9d861a12f8a5bd5977737f492a9e89bdb315d381
Esterling Accime <esterling@brainbytescreative.com>
2017-12-05 00:11:35 -0500

    Redirecting after login or sign up to the Dashboard

--- server.js
+++ server.js
@@ -1,47 +1,152 @@
-var express = require("express");
-var app = express();
-var jwt = require('jsonwebtoken');
-var bodyParser = require("body-parser");
+
+// JSON Web Token = Secure routes API
 
 
 
-var port = process.env.PORT || 8080;
+// Authentication
+
+// Passport
+
+// Oauth0
+
+// Bcrypt
+
+// Oauth 2.0
+
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+
+
+// Configuration
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/test");
 app.use(bodyParser());
+
+
+// Model
+const UserSchema = mongoose.Schema({
+    username:{
+        type:String
+    },
+
+    password:{
+        type:String
+    }
+});
+
+const User = mongoose.model('user', UserSchema);
+
+
+
 
 
 
 // Routes
-
-// Generating token
-app.get('/', function (req, res) {
-	var user = {
-		username:'accimeesterlin',
-		age:23
-	};
-
-	jwt.sign(user, 'teachersecret', { expiresIn: '10h' }, function (err, token) {
-
-		if(err)
-			return err
-		res.json({token:token});
-	});
-	
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Verifying token
-app.get('/verify', function (req, res) {
-	
-	var token = req.param('token');
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, "signup.html"));
+});
 
-	var data = jwt.verify(token, 'secretforever', function (err, user) {
-		if(err)
-			res.json({token:'expire'});
-		res.json({user:user});
-	});
+app.get('/signin', (req, res) => {
+    res.sendFile(path.join(__dirname, "signin.html"));
+});
 
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, "dashboard.html"));
 });
 
 
-app.listen(port, function () {
-	console.log("App is starting");
+// Handling Authentication
+app.post('/signup', (req, res) => {
+    const user = req.body; // accimeesterlin
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(user.password, salt);
+
+    console.log("Hash: ", salt);
+
+    User.findOne({username: user.username})
+        .then((currentUser) => {
+            if(currentUser){
+                res.redirect('/signup');
+            } else{
+                let newUser = User({
+                    username:user.username,
+                    password: hash
+                });
+
+                newUser.save((err) => {
+                    if(err)
+                        res.redirect('/signup');
+                    res.redirect('/dashboard');
+                });
+            }
+        });
 });
+
+
+app.post('/signin', (req, res) => {
+
+    const user = req.body;
+    User.findOne({username: user.username})
+        .then((currentUser) => {
+            console.log(currentUser.password);
+
+            if(!currentUser){
+                res.redirect('/signin');
+            }
+
+            if(!bcrypt.compareSync(user.password, currentUser.password)){
+                res.redirect('/signin')
+            }
+
+            else{
+                res.redirect('/dashboard');
+            }
+
+        });
+});
+
+
+
+
+
+
+
+
+
+// Starting server
+app.listen(port, () => {
+    console.log("App is starting at port", port);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
